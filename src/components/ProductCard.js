0fdc6d45529f5d44f4ffcebafc8706e3bab7f444
MD Saimur Rahman Swarnab <Saimur101@gmail.com>
2024-08-22 19:58:00 +0600

    ProductCard.js

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,6 +1,14 @@
+export const ProductCard = ({product}) => {
+  const {name, price, image} = product;
 
-export const ProductCard = () => {
   return (
-    <div>ProductCard</div>
+    <div className="productCard">
+      <img src={image} alt={name} />
+      <p className="name">{name}</p>
+      <div className="action">
+        <p>{price}</p>
+        <button>Add To Cart</button>
+      </div>
+    </div>
   )
 }
