ba91aa38bad99bc2cb2865beced56223ae195420
MD Saimur Rahman Swarnab <Saimur101@gmail.com>
2024-08-22 19:57:50 +0600

    App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
+import { AllRoutes } from "./routes/AllRoutes";
+import { Header } from "./components";
 import './App.css';
-import { Header } from './components';
-import { AllRoutes } from './routes/AllRoutes';
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <AllRoutes/>
+      <Header />
+      <AllRoutes />
     </div>
   );
 }
