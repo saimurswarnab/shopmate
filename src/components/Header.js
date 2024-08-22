4b9fbc6955556cce27b68a746b0f464e30c7fadc
MD Saimur Rahman Swarnab <Saimur101@gmail.com>
2024-08-22 19:27:53 +0600

    Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,26 @@
+import {Link, NavLink} from "react-router-dom";
+import "./Header.css";
+import Logo from "../assests/logo.png"
+
 export const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <Link to="/" className="logo">
+        <img src={Logo} alt="Shopmate Logo" />
+        <span>Shopping Cart</span>
+      
+      </Link>
+
+      <nav className="navigation">
+        <NavLink to="/" className="link" >Home</NavLink>
+        <NavLink to="/cart" className="link">Cart</NavLink>
+      </nav>
+
+      <Link to="/cart" className="items">
+        <span>Cart:2</span>
+      
+      </Link>
+
+    </header>
   )
 }
