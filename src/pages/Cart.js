8fad5a391b8622776dc766239f60865eb20c9503
MD Saimur Rahman Swarnab <Saimur101@gmail.com>
2024-08-25 18:52:53 +0600

    Cart

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,6 +1,21 @@
 
+import { CartCard } from './../components/CartCard';
 export const Cart = () => {
+  const products =[
+
+      {"id": 1, "name": "Sony Wh-Ch510 Bluetooth Wireless", "price": 149, "image": "/assests/images/1001.png"},
+      {"id": 2, "name": "boAt Rockerz 450", "price": 49, "image": "/assests/images/1002.png"},
+  ]
   return (
-    <div>Cart</div>
+    <main>
+      <section className='cart'>
+        <h1>Cart Items:{products.length}</h1>
+        {products.map((product)=>(
+            <CartCard key={product.id} product={product}/>
+
+        ))}
+        
+      </section>
+    </main>
   )
 }
